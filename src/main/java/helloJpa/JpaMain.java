b2cfd37f2d6503d308490e89904988a4b074a487
SuyeonSun <suninisy@gmail.com>
2023-04-24 21:05:47 +0900

    프로젝트 빌드

---
+++ src/main/java/helloJpa/JpaMain.java
@@ -0,0 +1,14 @@
+package helloJpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        em.clear();
+        emf.close();
+    }
+}
