f5081a401e2d6eaf4e2b9e0c677fc0fad8752b37
SuyeonSun <suninisy@gmail.com>
2023-04-25 13:22:07 +0900

    필드와 컬럼 매핑

--- src/main/java/helloJpa/JpaMain.java
+++ src/main/java/helloJpa/JpaMain.java
@@ -17,8 +17,9 @@ public class JpaMain {
         try {
             // 엔티티 객체 생성. 아직 영속성 컨텍스트에 저장하지 않은 상태
             Member member = new Member();
-            member.setId(1L);
-            member.setName("Name1");
+            member.setRoleType(RoleType.ADMIN);
+            member.setUsername("Kim");
+            member.setId(100L);
             // 영속성 컨텍스트 persistence context: 엔티티를 영구 저장하는 환경
             // 엔티티 매니저는 영속성 컨텍스트에 엔티티 보관 및 관리
             // 영속성 컨텍스트에 저장된 영속 상태
--- src/main/java/helloJpa/Member.java
+++ src/main/java/helloJpa/Member.java
@@ -1,9 +1,8 @@
 package helloJpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
 // @Table(name = "Members") // Members라는 테이블에 셀행하라
@@ -12,7 +11,31 @@ public class Member {
     private Long id;
 
     // @Column(name = "userName") // userName이라는 컬럼에 매핑하라
-   private String name;
+    @Column(name = "name", nullable = false, length = 10)
+    private String username;
+
+    private Integer age;
+
+    // enum 타입이 없기 때문에 enum 타입 명시 시에는 @Enumerated 사용
+    // EnumType.ORDINARY 숫자
+    // EnumType.STRING 문자
+    // 필수로 STRING!
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    private LocalDate createdDate;
+
+    private LocalDate lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    // 특정 필드를 DB 말고 메모리에서만 사용하고 싶을 경우
+    @Transient
+    private int temp;
+
+    public Member() {}
+
 
     public Long getId() {
         return id;
@@ -22,12 +45,59 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDate getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDate lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
 }
